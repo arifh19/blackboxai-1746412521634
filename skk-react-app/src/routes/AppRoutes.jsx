facaef7d63629bf9e8272ff2cc71303bf5cf8128
Arif Hidayat <30359844+arifh19@users.noreply.github.com>
2025-05-05 16:14:02 +0700

    Add skk-react-app/src/routes/AppRoutes.jsx 1746436442829

--- skk-react-app/src/routes/AppRoutes.jsx
+++ skk-react-app/src/routes/AppRoutes.jsx
@@ -5,10 +5,24 @@ import Login from '../auth/Login';
 import AdminDashboard from '../views/AdminDashboard';
 import UserDashboard from '../views/UserDashboard';
 import ChangePassword from '../views/ChangePassword';
+import Home from '../views/Home';
+import TentangKami from '../views/TentangKami';
+import StrukturOrganisasi from '../views/StrukturOrganisasi';
 import DaftarLSP from '../views/DaftarLSP';
 import DaftarAsosiasi from '../views/DaftarAsosiasi';
 import DaftarLembagaDiklat from '../views/DaftarLembagaDiklat';
-import Home from '../views/Home';
+import RegulasiInternal from '../views/RegulasiInternal';
+import RegulasiEksternal from '../views/RegulasiEksternal';
+import Agenda from '../views/Agenda';
+import PengajuanSKKK from '../views/PengajuanSKKK';
+import PengajuanSKKI from '../views/PengajuanSKKI';
+import PengajuanSKKNI from '../views/PengajuanSKKNI';
+import PengajuanLSP from '../views/PengajuanLSP';
+import Dokumen from '../views/Dokumen';
+import Survey from '../views/Survey';
+import Galeri from '../views/Galeri';
+import Berita from '../views/Berita';
+import HubungiKami from '../views/HubungiKami';
 
 const ProtectedRoute = ({ children, requiredSubject }) => {
   const { user, loading } = useContext(AuthContext);
@@ -38,6 +52,23 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/tentang-kami" element={<TentangKami />} />
+        <Route path="/struktur-organisasi" element={<StrukturOrganisasi />} />
+        <Route path="/daftar-lsp" element={<DaftarLSP />} />
+        <Route path="/daftar-asosiasi" element={<DaftarAsosiasi />} />
+        <Route path="/daftar-lembaga-diklat" element={<DaftarLembagaDiklat />} />
+        <Route path="/regulasi-internal" element={<RegulasiInternal />} />
+        <Route path="/regulasi-eksternal" element={<RegulasiEksternal />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/pengajuan-skkk" element={<PengajuanSKKK />} />
+        <Route path="/pengajuan-skki" element={<PengajuanSKKI />} />
+        <Route path="/pengajuan-skkni" element={<PengajuanSKKNI />} />
+        <Route path="/pengajuan-lsp" element={<PengajuanLSP />} />
+        <Route path="/dokumen" element={<Dokumen />} />
+        <Route path="/survey" element={<Survey />} />
+        <Route path="/galeri" element={<Galeri />} />
+        <Route path="/berita" element={<Berita />} />
+        <Route path="/hubungi-kami" element={<HubungiKami />} />
         <Route path="/login" element={<Login />} />
 
         <Route
@@ -66,21 +97,6 @@ const AppRoutes = () => {
             </ProtectedRoute>
           }
         />
-
-        <Route path="/daftar-lsp" element={<DaftarLSP />} />
-        <Route path="/daftar-asosiasi" element={<DaftarAsosiasi />} />
-        <Route path="/daftar-lembaga-diklat" element={<DaftarLembagaDiklat />} />
-
-        <Route path="/regulasi-internal" element={<RegulasiInternal />} />
-        <Route path="/regulasi-eksternal" element={<RegulasiEksternal />} />
-        <Route path="/pengajuan-skkk" element={<PengajuanSKKK />} />
-        <Route path="/pengajuan-skki" element={<PengajuanSKKI />} />
-        <Route path="/pengajuan-skkni" element={<PengajuanSKKNI />} />
-        <Route path="/pengajuan-lsp" element={<PengajuanLSP />} />
-        <Route path="/dokumen" element={<Dokumen />} />
-        <Route path="/galeri" element={<Galeri />} />
-        <Route path="/berita" element={<Berita />} />
-        <Route path="/hubungi-kami" element={<HubungiKami />} />
 
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
