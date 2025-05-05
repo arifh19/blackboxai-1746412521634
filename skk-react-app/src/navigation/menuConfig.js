854a270f08e41fddfbc4b0e6619f598d432eaeff
Arif Hidayat <30359844+arifh19@users.noreply.github.com>
2025-05-05 15:35:14 +0700

    Add skk-react-app/src/navigation/menuConfig.js 1746434114414

---
+++ skk-react-app/src/navigation/menuConfig.js
@@ -0,0 +1,13 @@
+export const adminMenuItems = [
+  { name: 'Dashboard', path: '/admin-dashboard', subject: 'dashboard' },
+  { name: 'Daftar LSP', path: '/daftar-lsp', subject: 'daftarLSP' },
+  { name: 'Daftar Asosiasi', path: '/daftar-asosiasi', subject: 'daftarAsosiasi' },
+  { name: 'Daftar Lembaga Diklat', path: '/daftar-lembaga-diklat', subject: 'daftarLembagaDiklat' },
+];
+
+export const userMenuItems = [
+  { name: 'Dashboard', path: '/user-dashboard', subject: 'dashboard' },
+  { name: 'Daftar LSP', path: '/daftar-lsp', subject: 'daftarLSP' },
+  { name: 'Daftar Asosiasi', path: '/daftar-asosiasi', subject: 'daftarAsosiasi' },
+  { name: 'Daftar Lembaga Diklat', path: '/daftar-lembaga-diklat', subject: 'daftarLembagaDiklat' },
+];
