c814f993fa695f484fbf454f5dafb35232b3d611
Arif Hidayat <30359844+arifh19@users.noreply.github.com>
2025-05-06 09:08:19 +0700

    Add skk-react-app/src/navigation/menuConfig.js 1746497300360

--- skk-react-app/src/navigation/menuConfig.js
+++ skk-react-app/src/navigation/menuConfig.js
@@ -1,5 +1,14 @@
 export const adminMenuItems = [
   { name: 'Dashboard', path: '/admin-dashboard', subject: 'dashboard' },
+  {
+    name: 'Data Master',
+    subject: 'dataMaster',
+    subMenu: [
+      { name: 'Data LSP', path: '/admin/data-lsp', subject: 'dataLSP' },
+      { name: 'Data Asosiasi', path: '/admin/data-asosiasi', subject: 'dataAsosiasi' },
+      { name: 'Data LemDiklat', path: '/admin/data-lemdiklat', subject: 'dataLemdiklat' },
+    ],
+  },
   { name: 'Daftar LSP', path: '/daftar-lsp', subject: 'daftarLSP' },
   { name: 'Daftar Asosiasi', path: '/daftar-asosiasi', subject: 'daftarAsosiasi' },
   { name: 'Daftar Lembaga Diklat', path: '/daftar-lembaga-diklat', subject: 'daftarLembagaDiklat' },
